3e47a0e231b35b357838b67bad8d2077445b3d3e
Imran Khan <imranqau5373@gmail.com>
2019-12-07 08:15:29 +0500

    new comiit

--- routes/cardskipper.js
+++ routes/cardskipper.js
@@ -82,8 +82,8 @@ request(options, function (error, response, body) {
 
 function getTodayDate(){
     var today = new Date();
-var dd = String(today.getDate()).padStart(2, '0');
-var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+var dd = String(today.getDate()).padStart('2', '0');
+var mm = String(today.getMonth() + 1).padStart('2', '0'); //January is 0!
 var yyyy = today.getFullYear();
 
 today = yyyy + '-' + mm + '-' + dd;
